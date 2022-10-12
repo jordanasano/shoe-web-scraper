dc82becc8794e8b33ab5b3502855a28df1e8322d
Jordan Asano <jordanasano@yahoo.com>
2022-10-12 11:05:39 -0700

    Accounted for running out of shoes and cleaned up code

--- scraper.py
+++ scraper.py
@@ -3,11 +3,16 @@ import requests
 
 def scrape_shoe_data(num_shoes):
     count = 0
+
     while count < num_shoes:
         html_text = requests.get(f'https://www.kohls.com/catalog/mens-shoes.jsp?CN=Gender:Mens+Department:Shoes&cc=mens-TN2.0-S-shoes&kls_sbp=56751120711696885714221506018546775523&PPP=48&WS={count}&S=1&sks=true').text
         soup = BeautifulSoup(html_text, 'lxml')
 
         shoes = soup.find_all('li', class_='products_grid')
+
+        if len(shoes) == 0:
+            print("All out of shoes!")
+            return
 
         for shoe in shoes:
             if count == num_shoes:
@@ -29,5 +34,9 @@ def scrape_shoe_data(num_shoes):
             if price == '':
                 price = shoe.find('div', class_="prod_price_original").text.strip()
                 price += ', but add to cart to see reduced price'
+                
             count += 1
+            
             print(f"{count}: The {title} costs {price}.\n")
+
+scrape_shoe_data(4000)
